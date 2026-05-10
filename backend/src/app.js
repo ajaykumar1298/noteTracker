eceae98461f42f42d8b3fe775e9a19465ed6645c
ajaykumar1298 <ajaykumarak1209@gmail.com>
2026-05-11 01:49:57 +0530

    some issue fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://note-tracker-snowy.vercel.app/",
+  "https://note-tracker-snowy.vercel.app",
 ];
 
 app.use(
