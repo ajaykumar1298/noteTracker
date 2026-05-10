c17ba3648804414e6e3845f7df5dc1d82213c245
ajaykumar1298 <ajaykumarak1209@gmail.com>
2026-05-11 01:36:26 +0530

    update uri

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // let uri = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
